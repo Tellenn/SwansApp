001de06b995f77c4206495ab0e8021adc28ea496
Perrin, Kyllian - KPE3824 <kyllian.perrin@viseo.com>
2018-12-06 10:06:50 +0100

    Added roll page

---
+++ src/pages/roll/roll.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RollPage } from './roll';
+
+@NgModule({
+  declarations: [
+    RollPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RollPage),
+  ],
+})
+export class RollPageModule {}
---
+++ src/pages/roll/roll.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-roll',
+  templateUrl: 'roll.html',
+})
+export class RollPage {
+  diceResult: number;
+  number: number;
+  face: number;
+  decomposedResult : number[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.decomposedResult = new Array<number>();
+  }
+  
+
+  result() {
+    this.decomposedResult = new Array<number>();
+    let sum : number = 0;
+    for (let i = 0; i < this.number; i++) {
+      this.decomposedResult.push(Math.floor(Math.random() * this.face) + 1);
+      sum += this.decomposedResult[i];
+    }
+    this.face < 1 ? this.diceResult = 0 : this.diceResult = sum;
+  }
+
+}
